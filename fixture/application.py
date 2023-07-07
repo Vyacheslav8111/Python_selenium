c350864e24837c3d7ae11da3288d72d853080037
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-07 18:57:14 +0300

    Оптимизация переходов по страницам приложения. Добавлены проверки в класс GroupHelper.

--- fixture/application.py
+++ fixture/application.py
@@ -7,7 +7,7 @@ from fixture.contact import ContactHelper
 class Application:
 	def __init__(self):
 		self.driver = webdriver.Chrome()
-		self.driver.implicitly_wait(5)
+		#self.driver.implicitly_wait(5)
 		self.session = SessionHelper(self)
 		self.group = GroupHelper(self)
 		self.contact = ContactHelper(self)
