6910110b2e74b6c6ad35e5b5fbfab953b9ace533
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-07 11:44:17 +0300

    В фикстуру добавлена проверка валидности сессии браузера.

--- fixture/application.py
+++ fixture/application.py
@@ -6,10 +6,17 @@ from fixture.contact import ContactHelper
 
 class Application:
 	def __init__(self):
-		self.driver = webdriver.Chrome()
+		self.driver = webdriver.Firefox()
 		self.session = SessionHelper(self)
 		self.group = GroupHelper(self)
 		self.contact = ContactHelper(self)
+
+	def is_valid(self):
+		try:
+			self.driver.current_url
+			return True
+		except:
+			return False
 
 	def open_home_page(self):
 		driver = self.driver
