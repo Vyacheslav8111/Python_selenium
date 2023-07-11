47d1003b8714ff50d928a812c34e52f2119c081a
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-11 14:46:34 +0300

    Добавлены проверки на равенство списков контактов в тесты  test_modify_contact_first_name и test_modify_contact_fax.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,10 +4,17 @@ from model.contact import Contact
 def test_modify_contact_first_name(app):
 	if app.contact.count_contact() == 0:
 		app.contact.create(Contact(firstname="New_middlename"))
+	old_contacts = app.contact.get_contact_list()
 	app.contact.modify_first_contact(Contact(firstname="fax_455657"))
+	new_contacts = app.contact.get_contact_list()
+	assert len(old_contacts) == len(new_contacts)
 
 
 def test_modify_contact_fax(app):
 	if app.contact.count_contact() == 0:
 		app.contact.create(Contact(firstname="ryty"))
+	old_contacts = app.contact.get_contact_list()
 	app.contact.modify_first_contact(Contact(firstname="New title"))
+	new_contacts = app.contact.get_contact_list()
+	assert len(old_contacts) == len(new_contacts)
+
