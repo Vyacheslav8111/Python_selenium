340613c065a02722c054aacfa011dc871539e7cd
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-11 14:43:30 +0300

    Добавлены проверки на равенство списков контактов в тесты  test_delete_first_contact.

--- test/test_delete_contact.py
+++ test/test_delete_contact.py
@@ -4,4 +4,7 @@ from model.contact import Contact
 def test_delete_first_contact(app):
 	if app.contact.count_contact() == 0:
 		app.contact.create(Contact(firstname="New_middlename"))
+	old_contacts = app.contact.get_contact_list()
 	app.contact.delete_first_contact()
+	new_contacts = app.contact.get_contact_list()
+	assert len(old_contacts) - 1 == len(new_contacts)
