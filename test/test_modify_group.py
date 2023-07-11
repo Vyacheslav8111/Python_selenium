052f0e32eea50319c214cdfc4d93f1646592000a
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-11 13:18:25 +0300

    Добавлены проверки на равенство списков групп в тесты  modify_group_name.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -4,10 +4,16 @@ from model.group import Group
 def test_modify_group_name(app):
 	if app.group.count_group() == 0:
 		app.group.create(Group(header="test_header"))
+	old_groups = app.group.get_group_list()
 	app.group.modify_first_group(Group(name="New name"))
+	new_groups = app.group.get_group_list()
+	assert len(old_groups) == len(new_groups)
 
 
 def test_modify_group_header(app):
 	if app.group.count_group() == 0:
 		app.group.create(Group(footer="test_footer"))
+	old_groups = app.group.get_group_list()
 	app.group.modify_first_group(Group(header="New footer"))
+	new_groups = app.group.get_group_list()
+	assert len(old_groups) == len(new_groups)
