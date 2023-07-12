6dbf3be922f3252dfb0b1076c290ea3f3370e7fc
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-12 22:10:38 +0300

    Добавлена полноценная проверки на равенство списков контактов в тесты  test_add_contact  а не только размеров списков.

--- test/test_contact_creation.py
+++ test/test_contact_creation.py
@@ -4,20 +4,18 @@ from model.contact import Contact
 
 def test_add_contact(app):
 	old_contacts = app.contact.get_contact_list()
-	app.contact.create(Contact(firstname="q", middlename="w", lastname="e", nickname="r", title="t",
-					   company="y",
-					   address="u", home="67767-8778", mobile="56565-325", work="6688", fax="5657678",
-					   email="test@test.test", email2="test123@test.test", email3="test78@test1.test",
-					   homepage="www.testUI.test", address2="test", notes="test"))
+	contact = Contact(firstname="q")
+	app.contact.create(contact)
 	new_contacts = app.contact.get_contact_list()
 	assert len(old_contacts) + 1 == len(new_contacts)
+	old_contacts.append(contact)
 
 
-def test_add_empty_contact(app):
-	old_contacts = app.contact.get_contact_list()
-	app.contact.create(Contact(firstname="", middlename="", lastname="", nickname="", title="", company="",
-					   address="", home="", mobile="", work="", fax="",
-					   email="", email2="", email3="",
-					   homepage="", address2="", notes=""))
-	new_contacts = app.contact.get_contact_list()
-	assert len(old_contacts) + 1 == len(new_contacts)
+	assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key= Contact.id_or_max)
+
+
+# def test_add_empty_contact(app):
+# 	old_contacts = app.contact.get_contact_list()
+# 	app.contact.create(Contact(firstname=""))
+# 	new_contacts = app.contact.get_contact_list()
+# 	assert len(old_contacts) + 1 == len(new_contacts)
