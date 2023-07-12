c3c7af8aff6e176a14ae45b2f69b3213c8ff36f5
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-12 09:22:53 +0300

    Добавлена полноценная проверки на равенство списков групп в тесты  test_modify_group_name  а не только размеров списков.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,15 +5,19 @@ def test_modify_group_name(app):
 	if app.group.count_group() == 0:
 		app.group.create(Group(header="test_header"))
 	old_groups = app.group.get_group_list()
+	group = Group(name="New name")
+	group.id = old_groups[0].id
 	app.group.modify_first_group(Group(name="New name"))
 	new_groups = app.group.get_group_list()
 	assert len(old_groups) == len(new_groups)
+	old_groups[0] = group
+	assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
-def test_modify_group_header(app):
-	if app.group.count_group() == 0:
-		app.group.create(Group(footer="test_footer"))
-	old_groups = app.group.get_group_list()
-	app.group.modify_first_group(Group(header="New footer"))
-	new_groups = app.group.get_group_list()
-	assert len(old_groups) == len(new_groups)
+# def test_modify_group_header(app):
+# 	if app.group.count_group() == 0:
+# 		app.group.create(Group(footer="test_footer"))
+# 	old_groups = app.group.get_group_list()
+# 	app.group.modify_first_group(Group(header="New footer"))
+# 	new_groups = app.group.get_group_list()
+# 	assert len(old_groups) == len(new_groups)
