6e12d408f15c6c2c7c19a0bdc2e3d92c38a058f9
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-13 09:18:15 +0300

    Добавлена полноценная проверки на равенство списков контактов в тесты  test_modify_contact_first_name  а не только размеров списков.

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,16 +5,20 @@ def test_modify_contact_first_name(app):
 	if app.contact.count_contact() == 0:
 		app.contact.create(Contact(firstname="New_middlename"))
 	old_contacts = app.contact.get_contact_list()
-	app.contact.modify_first_contact(Contact(firstname="fax_455657"))
+	contact = Contact(firstname="fax_455657")
+	contact.id = old_contacts[0].id
+	app.contact.modify_first_contact(contact)
 	new_contacts = app.contact.get_contact_list()
 	assert len(old_contacts) == len(new_contacts)
+	old_contacts[0] = contact
+	assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 
-def test_modify_contact_fax(app):
-	if app.contact.count_contact() == 0:
-		app.contact.create(Contact(firstname="ryty"))
-	old_contacts = app.contact.get_contact_list()
-	app.contact.modify_first_contact(Contact(firstname="New title"))
-	new_contacts = app.contact.get_contact_list()
-	assert len(old_contacts) == len(new_contacts)
+# def test_modify_contact_fax(app):
+# 	if app.contact.count_contact() == 0:
+# 		app.contact.create(Contact(firstname="ryty"))
+# 	old_contacts = app.contact.get_contact_list()
+# 	app.contact.modify_first_contact(Contact(firstname="New title"))
+# 	new_contacts = app.contact.get_contact_list()
+# 	assert len(old_contacts) == len(new_contacts)
 
