1ab328a3c534e85a9f43e01ba40b4d5191c64096
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-14 11:28:55 +0300

    Доработан класс Contact.

--- model/contact.py
+++ model/contact.py
@@ -26,13 +26,12 @@ class Contact:
 		self.id = id
 
 	def __repr__(self):
-		return "%s:%s" % (self.id, self.firstname)
+		return "%s:%s %s" % (self.id, self.firstname, self.lastname)
 
 	def __eq__(self, other):
-		return (self.id is None or self.firstname is None or other.firstname or self.lastname is None or
-				other.lastname or other.id is None or self.id == other.id) and \
+		return (self.id is None or other.lastname or other.id is None or self.id == other.id) and \
 			self.firstname == \
-			other.firstname or\
+			other.firstname and\
 			self.lastname == other.lastname
 
 	def id_or_max(self):
