026b9339168c8d52889e57f70c0aaff5a7fceb16
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-14 11:20:59 +0300

    Оптимизация работа со строками в классе SessionHelper.

--- fixture/session.py
+++ fixture/session.py
@@ -26,7 +26,11 @@ class SessionHelper:
 
 	def is_logged_in_as(self, username):
 		driver = self.app.driver
-		return driver.find_element(By.XPATH, "//div/div[1]/form/b").text == "("+username+")"
+		return self.logged_user() == username
+
+	def logged_user(self):
+		driver = self.app.driver
+		return driver.find_element(By.XPATH, "//div/div[1]/form/b").text[1:-1]
 
 	def ensure_logout(self):
 		driver = self.app.driver
