c861b07e81b74369a705f4518ec32a9f85f131db
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-21 21:54:25 +0300

    доработан тест test_contact_info.py

--- model/contact.py
+++ model/contact.py
@@ -2,12 +2,10 @@ from sys import maxsize
 
 
 class Contact:
-	def __init__(self, id=None, firstname=None, lastname=None, address=None, address2=None, home_phone=None, \
-				 mobile_phone=None, \
-				 work_phone=None,
+	def __init__(self, id=None, firstname=None, lastname=None, address=None, address2=None, home_phone=None,
+				 mobile_phone=None, work_phone=None,
 				 secondary_phone=None, all_phones_from_home_page=None, all_phones_from_view_page=None,
 				 all_emails_from_home_page=None, all_main_info_from_home_page=None):
-		self.id = id
 		self.firstname = firstname
 		self.lastname = lastname
 		self.address = address
@@ -20,15 +18,14 @@ class Contact:
 		self.all_phones_from_view_page = all_phones_from_view_page
 		self.all_emails_from_home_page = all_emails_from_home_page
 		self.all_main_info_from_home_page = all_main_info_from_home_page
+		self.id = id
 
 	def __repr__(self):
-		return "%s:%s:%s:%s:%s:%s:%s:%s" % (self.id, self.lastname, self.firstname, self.address, self.home_phone,
-											self.mobile_phone,
-											self.work_phone, self.secondary_phone)
+		return "%s:%s:%s" % (self.id, self.lastname, self.firstname)
 
 	def __eq__(self, other):
 		return (self.id is None or other.id is None or self.id == other.id) and \
-			self.lastname == other.lastname and self.firstname == other.firstname and self.address == other.address and self.all_phones_from_home_page == other.all_phones_from_home_page and self.all_emails_from_home_page == other.all_emails_from_home_page
+			self.lastname == other.lastname and self.firstname == other.firstname
 
 	def id_or_max(self):
 		if self.id:
--- test/test_contact_creation.py
+++ test/test_contact_creation.py
@@ -4,13 +4,13 @@ from model.contact import Contact
 
 def test_add_contact(app):
 	old_contacts = app.contact.get_contact_list()
-	contact = Contact(firstname="b", lastname="b")
+	contact = Contact(firstname="new_f_n", lastname="new_l_n", home_phone="123")
 	app.contact.create(contact)
-	assert len(old_contacts) + 1 == app.contact.count_contact()# .count - хеш-функция
+	assert len(old_contacts) + 1 == app.contact.count_contact()  # .count - хеш-функция
 	new_contacts = app.contact.get_contact_list()
 	old_contacts.append(contact)
-	assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key= Contact.id_or_max)
 
+	assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
 
 # def test_add_empty_contact(app):
 # 	old_contacts = app.contact.get_contact_list()
