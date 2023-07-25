df3cfa07e38155de0439e9fe5da55bc069c90eb2
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-25 18:13:57 +0300

    DDT генерация тестовых данных.Продолжение. Генерация комбинаций данных(Pairwaise).Добавлены символы пунктуации в тестовые данные.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -7,7 +7,7 @@ import random
 import string
 
 def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits + " "*10
+    symbols = string.ascii_letters + string.digits + string.punctuation + " " * 10
     return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
@@ -22,12 +22,12 @@ testdata = [
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_group(app, group):
     pass
-    # old_groups = app.group.get_group_list()
-    # app.group.create(group)
-    # assert len(old_groups) + 1 == app.group.count_group() # .count - хеш-функция
-    # new_groups = app.group.get_group_list()
-    # old_groups.append(group)
-    # assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    old_groups = app.group.get_group_list()
+    app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count_group() # .count - хеш-функция
+    new_groups = app.group.get_group_list()
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 # def test_add_empty_group(app):
