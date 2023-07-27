1ee2ca57e9b4bca4807e3012d85fa8ae914cf9bc
Vyacheslav8111 <saper.8111@gmail.com>
2023-07-27 20:29:01 +0300

    Избавились от указания директории запуска тестов в настройках PyCharm.

--- conftest.py
+++ conftest.py
@@ -1,20 +1,24 @@
 import pytest
 from fixture.application import Application
+import json
+import os.path
 
 fixture = None
+target = None
 
 
 @pytest.fixture
 def app(request):
     global fixture
+    global target
     browser = request.config.getoption("--browser")
-    base_url = request.config.getoption("--baseUrl")
-    if fixture is None:
-        fixture = Application(browser=browser, base_url=base_url)
-    else:
-        if not fixture.is_valid():
-            fixture = Application(browser=browser, base_url=base_url)
-    fixture.session.ensure_login(username="Admin", password="secret")
+    if target is None:
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
+        with open(config_file) as f:
+            target = json.load(f)
+    if fixture is None or not fixture.is_valid():
+        fixture = Application(browser=browser, base_url=target["baseUrl"])
+    fixture.session.ensure_login(username=target["username"], password=target["password"])
     return fixture
 
 
